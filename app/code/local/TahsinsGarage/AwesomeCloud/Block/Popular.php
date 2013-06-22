57cb7b7d4fc1e70592efdde0331243eb0b519074
Tahsin Hasan <tahsin@grameentek.com>
2013-06-22 18:54:15 +0600

    s

---
+++ app/code/local/TahsinsGarage/AwesomeCloud/Block/Popular.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class TahsinsGarage_AwesomeCloud_Block_Popular extends Mage_Tag_Block_Popular
+{
+    
+}
