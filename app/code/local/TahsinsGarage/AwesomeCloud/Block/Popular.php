443f1fb6ed80a1f6a7c4777073e56480715e279f
Tahsin Hasan <tahsin@grameentek.com>
2013-06-23 18:37:29 +0600

    s

--- app/code/local/TahsinsGarage/AwesomeCloud/Block/Popular.php
+++ app/code/local/TahsinsGarage/AwesomeCloud/Block/Popular.php
@@ -5,7 +5,12 @@
  * and open the template in the editor.
  */
 
-class TahsinsGarage_AwesomeCloud_Block_Popular extends Mage_Tag_Block_Popular
-{
-    
+class TahsinsGarage_AwesomeCloud_Block_Popular extends Mage_Tag_Block_Popular {
+
+    protected function _prepareLayout() {
+        $this->getLayout()->getBlock('head')->addJs('awesomecloud/jquery.awesomeCloud-0.2.js');
+
+        return parent::_prepareLayout();
+    }
+
 }
